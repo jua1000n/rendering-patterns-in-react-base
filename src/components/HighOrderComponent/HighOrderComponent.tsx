0f8b05486bc4815f8722267063db6e043ece69cd
Juan Cadavid <desarrollador@polloandino.com>
2025-04-07 11:25:06 -0500

    add high order component

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,27 @@
+import React from "react";
+
+function withLoading<T extends object>(Component: React.ComponentType<T>) {
+  return (props: T & { isLoading: boolean }) => {
+    const { isLoading, ...rest } = props;
+    return isLoading ? <p>Loading...</p> : <Component {...(rest as T)} />;
+  };
+}
+
+type UserProps = {
+  name: string;
+};
+
+const UserComponen: React.FC<UserProps> = ({ name }) => {
+  return <p>Hello, {name}!</p>;
+};
+
+const UserWithLoading = withLoading(UserComponen);
+
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading isLoading={false} name="Pepe" />
+      <UserWithLoading isLoading={true} name="Arturo" />
     </div>
   );
 };
