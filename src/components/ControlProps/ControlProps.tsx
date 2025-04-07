5d106825f4f0111f11b8a11225a507ee48f36adc
Juan Cadavid <desarrollador@polloandino.com>
2025-04-07 10:52:41 -0500

    toggle parent control to child

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+
+type ToggleProps = {
+  isToggled?: boolean;
+  onToggle?: (value: boolean) => void;
+};
+
+export const Toggle: React.FC<ToggleProps> = ({
+  isToggled = false,
+  onToggle,
+}) => {
+  const [stateToggle, setstateToggle] = useState<boolean>(isToggled);
+
+  const toggle = () => {
+    const newToggle = !stateToggle;
+    setstateToggle(newToggle);
+
+    if (onToggle) {
+      onToggle(newToggle);
+    }
+  };
+
+  return <button onClick={toggle}>{stateToggle ? "ON" : "OFF"}</button>;
+};
+
 export const ParentComponent = () => {
+  const [stateToggle, setStateToggle] = useState(false);
+
   return (
     <div>
+      Toggle is {stateToggle ? "ON" : "OFF"}
+      <Toggle isToggled={stateToggle} onToggle={setStateToggle} />
     </div>
   );
 };
