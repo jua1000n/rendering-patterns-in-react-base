5342cf747b5f1f56d81554c4b0f4e968ed257edd
Juan Cadavid <desarrollador@polloandino.com>
2025-04-07 11:55:54 -0500

    components controlled and uncontrolled

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("");
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Ingrese el codigo del cupon"
+        value={value}
+        onChange={(e) => {
+          setValue(e.currentTarget.value);
+        }}
+      />
+      <p>
+        Cupon de descuento <b>{value}</b>
+      </p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,17 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`Nuevo producto en el carrito: ${inputRef.current.value}`);
+    }
+  };
+
   return (
     <div>
+      <input ref={inputRef} type="text" placeholder="Ingrese el producto" />
+      <button onClick={handleSubmit}>Anadir al carrito</button>
     </div>
   );
 };
