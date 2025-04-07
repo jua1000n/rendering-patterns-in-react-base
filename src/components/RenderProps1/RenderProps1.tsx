70ff3378df5209bbead790d9956f444e9daa4887
Juan Cadavid <desarrollador@polloandino.com>
2025-04-07 10:42:58 -0500

    render props and composition

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,16 @@
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+};
+
+export const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name = "Teffcode";
+  return <div>{render(name)}</div>;
+};
+
 export const ParentComponent = () => {
   return (
     <>
+      <ChildComponent render={(name) => <p>Hello, {name}!</p>} />
     </>
   );
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,13 @@
-const ParentComponent = () => {
+import { useState } from "react";
+
+interface ParentComponentProps {
+  render: (data: string[]) => React.ReactNode;
+}
+
+const ParentComponent: React.FC<ParentComponentProps> = ({ render }) => {
+  const [data] = useState<string[]>(["Apple", "Banana", "Orange"]);
+
+  return <ul>{render(data)}</ul>;
 };
 
 export default ParentComponent;
