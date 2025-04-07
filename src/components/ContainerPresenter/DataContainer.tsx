b571d2ba57fbe0990ae693683c23c91b83e99cd6
Juan Cadavid <desarrollador@polloandino.com>
2025-04-07 11:47:03 -0500

    container-presenter

--- src/components/ContainerPresenter/DataContainer.tsx
+++ src/components/ContainerPresenter/DataContainer.tsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
+import { DataItem, DataPresenter } from "./DataPresenter";
+
 export const DataContainer = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("/data/data.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(`Error al cargar los datos: ${error}`);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
+      {loading && <p>Cargando...</p>}
+      {error && <p>{error}</p>}
+      <DataPresenter data={data} />
     </div>
   );
 };
